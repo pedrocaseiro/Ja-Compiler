857588a8f27c8b4acf5419b86c5bb341daf7198a
Daniel Azevedo <daniel@DanielDebian.home>
2017-02-25 15:54:27 +0000

    test maker

---
+++ tests/test_maker.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	FILE *f = fopen("test1.txt", "w");
+	if (f == NULL){
+	    printf("Error opening file!\n");
+	}
+
+
+	fprintf(f,"dadada\ndadadad");
+
+}
